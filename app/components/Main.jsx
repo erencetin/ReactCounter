1a5f96bc9f66e5ba1b32b8507b3aa8d94bea620a
erencetin <eecetin@gmail.com>
2017-07-19 10:00:43 -0400

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var Nav = require('Nav');
+var Main = (props) =>{
+  return (
+      <div>
+        <div>
+            <Nav/>
+            {props.children}
+        </div>
+      </div>
+    );
+}
+module.exports = Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var {Link,IndexLink} = require('react-router');
+
+var Nav = () =>
+    {
+            return(
+                <div className="top-bar">
+                    <div className="top-bar-left">
+                        <ul className="menu">
+                            <li className="menu-text">Counter</li>
+                            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</IndexLink></li>
+                            <li>
+                                <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> Count Down</Link>
+
+                            </li>
+                            <li>
+                                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}> About</Link>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="top-bar-right">
+                        <p className="menu-text">Eren Çetin</p>
+                    </div>
+                </div>
+            );
+        
+    };
+
+module.exports = Nav;
