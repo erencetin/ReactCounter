1d2754babddde5f08ccf5b52f2a0592d85c7e32f
erencetin <eecetin@gmail.com>
2017-07-21 10:40:16 -0400

    test are really added

---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,31 @@
+import React from  'react';
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $  = require('JQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Clock = require('Clock');
+
+describe('Clock',() =>{
+    it('should exist',()=>{
+        expect(Clock).toExist();
+    });
+
+    describe('formatSeconds',() =>{
+        it('should format seconds', ()=> {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+
+          it('should put zero on first letter when seconds less than 10', ()=> {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds);
+            expect(actual).toBe(expected);
+        });
+    });
+});
