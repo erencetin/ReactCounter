137b07e291ae78c1e016b107adabbc6588196378
erencetin <eecetin@gmail.com>
2017-08-11 10:50:00 -0400

    asdfasfd

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -10,6 +10,9 @@ var Countdown = React.createClass({
       countdownStatus: 'stopped'
     };
   },
+  componentWillUpdate:(nextProps,nextState)=>{
+
+  },
   componentDidUpdate: function (prevProps, prevState) {
     if (this.state.countdownStatus !== prevState.countdownStatus) {
       switch (this.state.countdownStatus) {
@@ -25,12 +28,21 @@ var Countdown = React.createClass({
       }
     }
   },
+
+  componentWillUnmount: ()=>{
+
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function () {
     this.timer = setInterval(() => {
       var newCount = this.state.count - 1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+      if (newCount === 0) {
+        this.setState({countdownStatus:'stopped'});
+      }
     }, 1000);
   },
   handleSetCountdown: function (seconds) {
